e31cdd5566419b9dc5bed2f245de870bc9f91855
UnknownGamerYT <kyriakosantoniou3@gmail.com>
2024-02-20 15:30:40 +0100

    Added game activity and some template images

---
+++ app/src/main/java/com/example/dreamteam/game_activity.java
@@ -0,0 +1,15 @@
+package com.example.dreamteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class game_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_activity);
+    }
+}
