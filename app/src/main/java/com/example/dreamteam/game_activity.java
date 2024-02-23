36598a9a3fcbe0c5372abc9236a7a04fc0e7e83e
LinasRaic <l.r.raicinskis@student.rug.nl>
2024-02-23 12:47:40 +0100

    more button functionality

--- app/src/main/java/com/example/dreamteam/game_activity.java
+++ app/src/main/java/com/example/dreamteam/game_activity.java
@@ -12,7 +12,7 @@ import com.example.dreamteam.game.DobbleGame;
 
 import java.util.Random;
 
-public class game_activity extends AppCompatActivity {
+public class game_activity extends AppCompatActivity implements View.OnClickListener{
     DobbleGame dobbleGame;
 
     @Override
@@ -21,15 +21,57 @@ public class game_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
         dobbleGame = new DobbleGame();
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        ImageButton imageButton2 = findViewById(R.id.imageButton2);
+        ImageButton imageButton3 = findViewById(R.id.imageButton3);
+        ImageButton imageButton4 = findViewById(R.id.imageButton4);
+        ImageButton imageButton5 = findViewById(R.id.imageButton5);
+        ImageButton imageButton6 = findViewById(R.id.imageButton6);
+        //ImageButton imageButton7 = findViewById(R.id.imageButton7);
+
+        imageButton.setOnClickListener(this);
+        imageButton2.setOnClickListener(this);
+        imageButton3.setOnClickListener(this);
+        imageButton4.setOnClickListener(this);
+        imageButton5.setOnClickListener(this);
+        imageButton6.setOnClickListener(this);
+        //imageButton7.setOnClickListener(this);
+
 
     }
 
-    public void Button1Press(View v) {
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.imageButton:
+                ButtonPressed(v, R.id.imageButton);
+                break;
+            case R.id.imageButton2:
+                ButtonPressed(v, R.id.imageButton2);
+                break;
+            case R.id.imageButton3:
+                ButtonPressed(v, R.id.imageButton3);
+                break;
+            case R.id.imageButton4:
+                ButtonPressed(v, R.id.imageButton4);
+                break;
+            case R.id.imageButton5:
+                ButtonPressed(v, R.id.imageButton5);
+                break;
+            case R.id.imageButton6:
+                ButtonPressed(v, R.id.imageButton6);
+                break;
+
+        }
+    }
+    public void ButtonPressed(View v, int buttonId){
         TextView text=(TextView)findViewById(R.id.ScoreText);
         String[] separated = text.getText().toString().split(" ");
         int value = Integer.parseInt(separated[1]);
-        ImageButton img= (ImageButton) findViewById(R.id.imageButton);
-        int[] images = dobbleGame.getFullImageList();//{androidx.constraintlayout.widget.R.drawable.abc_edit_text_material, androidx.transition.R.drawable.abc_list_focused_holo, R.drawable.wganniversary_background, R.drawable.ic_launcher_foreground};
+        ImageButton img= (ImageButton) findViewById(buttonId);
+        int[] images = dobbleGame.getFullImageList();
         Random random = new Random();
         img.setImageResource(images[random.nextInt(images.length)]);
         if (true)//if wins
@@ -46,4 +88,28 @@ public class game_activity extends AppCompatActivity {
 
         text.setText(newText);
     }
+    public void Button1Press(View v) {
+        TextView text=(TextView)findViewById(R.id.ScoreText);
+        String[] separated = text.getText().toString().split(" ");
+        int value = Integer.parseInt(separated[1]);
+        ImageButton img= (ImageButton) findViewById(R.id.imageButton);
+        int[] images = dobbleGame.getFullImageList();
+        Random random = new Random();
+        img.setImageResource(images[random.nextInt(images.length)]);
+        if (true)//if wins
+        {
+            value += 1;
+            Toast.makeText(getApplicationContext(), "Congratulations!", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            value-= 1;
+            Toast.makeText(getApplicationContext(), "Incorrect, point deducted", Toast.LENGTH_LONG).show();
+        }
+        String newText = "Score: " + value;
+
+        text.setText(newText);
+    }
+
+
 }
