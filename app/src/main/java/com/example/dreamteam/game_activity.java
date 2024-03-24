54952b648cc3e3ca985640e4245c9b5302157a4b
LinasRaic <l.r.raicinskis@student.rug.nl>
2024-03-24 03:03:14 +0100

    Added some countdown code but it is buggy with visualization and buttons.

--- app/src/main/java/com/example/dreamteam/game_activity.java
+++ app/src/main/java/com/example/dreamteam/game_activity.java
@@ -22,12 +22,14 @@ import java.util.List;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class game_activity extends AppCompatActivity implements View.OnClickListener {
     DobbleGame dobbleGame;
     boolean runModel = true;
     int[] bottomCardButtons = new int[] {R.id.imageButton,R.id.imageButton2,R.id.imageButton3,R.id.imageButton4,R.id.imageButton6,R.id.imageButton7,R.id.imageButton8,R.id.imageButton9};
     int[] topCardButtons = new int[] {R.id.imageButton10, R.id.imageButton11,R.id.imageButton12,R.id.imageButton13,R.id.imageButton15,R.id.imageButton16,R.id.imageButton17,R.id.imageButton18};
+    int[] allButtons = new int[] {R.id.imageButton,R.id.imageButton2,R.id.imageButton3,R.id.imageButton4,R.id.imageButton6,R.id.imageButton7,R.id.imageButton8,R.id.imageButton9,R.id.imageButton10, R.id.imageButton11,R.id.imageButton12,R.id.imageButton13,R.id.imageButton15,R.id.imageButton16,R.id.imageButton17,R.id.imageButton18};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //visual input, separate images into chunks, and put them into visicon.
@@ -110,6 +112,7 @@ public class game_activity extends AppCompatActivity implements View.OnClickList
         imageButton17.setOnClickListener(this);
         imageButton18.setOnClickListener(this);
         //imageButton7.setOnClickListener(this);
+        //Countdown();
         Update_Game();
 
 
@@ -118,64 +121,66 @@ public class game_activity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
+        //if one button is enabled all should be. had to add this because app would crash when button is clicked during countdown before first pair is drawn
+        ImageButton img1 = (ImageButton) findViewById(bottomCardButtons[0]);
+        if(img1.isEnabled())
+            switch (v.getId()) {
+                case R.id.imageButton:
+                    ButtonPressed(R.id.imageButton);
+                    break;
+                case R.id.imageButton2:
+                    ButtonPressed( R.id.imageButton2);
+                    break;
+                case R.id.imageButton3:
+                    ButtonPressed( R.id.imageButton3);
+                    break;
+                case R.id.imageButton4:
+                    ButtonPressed( R.id.imageButton4);
+                    break;
+                case R.id.imageButton5:
+                    ButtonPressed( R.id.imageButton5);
+                    break;
+                case R.id.imageButton6:
+                    ButtonPressed( R.id.imageButton6);
+                    break;
+                case R.id.imageButton7:
+                    ButtonPressed( R.id.imageButton7);
+                    break;
 
-        switch (v.getId()) {
+                case R.id.imageButton8:
+                    ButtonPressed( R.id.imageButton8);
+                    break;
+                case R.id.imageButton9:
+                    ButtonPressed( R.id.imageButton9);
+                    break;
+                case R.id.imageButton10:
+                    ButtonPressed( R.id.imageButton10);
+                    break;
+                case R.id.imageButton11:
+                    ButtonPressed( R.id.imageButton11);
+                    break;
+                case R.id.imageButton12:
+                    ButtonPressed( R.id.imageButton12);
+                    break;
+                case R.id.imageButton13:
+                    ButtonPressed( R.id.imageButton13);
+                    break;
+                case R.id.imageButton14:
+                    ButtonPressed( R.id.imageButton14);
+                    break;
+                case R.id.imageButton15:
+                    ButtonPressed( R.id.imageButton15);
+                    break;
+                case R.id.imageButton16:
+                    ButtonPressed( R.id.imageButton16);
+                    break;
+                case R.id.imageButton17:
+                    ButtonPressed( R.id.imageButton17);
+                    break;
+                case R.id.imageButton18:
+                    ButtonPressed( R.id.imageButton18);
+                    break;
 
-            case R.id.imageButton:
-                ButtonPressed(R.id.imageButton);
-                break;
-            case R.id.imageButton2:
-                ButtonPressed( R.id.imageButton2);
-                break;
-            case R.id.imageButton3:
-                ButtonPressed( R.id.imageButton3);
-                break;
-            case R.id.imageButton4:
-                ButtonPressed( R.id.imageButton4);
-                break;
-            case R.id.imageButton5:
-                ButtonPressed( R.id.imageButton5);
-                break;
-            case R.id.imageButton6:
-                ButtonPressed( R.id.imageButton6);
-                break;
-            case R.id.imageButton7:
-                ButtonPressed( R.id.imageButton7);
-                break;
-
-            case R.id.imageButton8:
-                ButtonPressed( R.id.imageButton8);
-                break;
-            case R.id.imageButton9:
-                ButtonPressed( R.id.imageButton9);
-                break;
-            case R.id.imageButton10:
-                ButtonPressed( R.id.imageButton10);
-                break;
-            case R.id.imageButton11:
-                ButtonPressed( R.id.imageButton11);
-                break;
-            case R.id.imageButton12:
-                ButtonPressed( R.id.imageButton12);
-                break;
-            case R.id.imageButton13:
-                ButtonPressed( R.id.imageButton13);
-                break;
-            case R.id.imageButton14:
-                ButtonPressed( R.id.imageButton14);
-                break;
-            case R.id.imageButton15:
-                ButtonPressed( R.id.imageButton15);
-                break;
-            case R.id.imageButton16:
-                ButtonPressed( R.id.imageButton16);
-                break;
-            case R.id.imageButton17:
-                ButtonPressed( R.id.imageButton17);
-                break;
-            case R.id.imageButton18:
-                ButtonPressed( R.id.imageButton18);
-                break;
 
         }
     }
@@ -195,6 +200,7 @@ public class game_activity extends AppCompatActivity implements View.OnClickList
         int symbolId = (Integer) img.getTag();//Integer.parseInt(
         if(cardPair.isMatchingSymbol(symbolId)){
             cardPair.solved = true;
+            //Countdown();
             Update_Game();
         }
 
@@ -273,7 +279,8 @@ public class game_activity extends AppCompatActivity implements View.OnClickList
         timeText.setText(timeToFind+"" );
 
         List<AttentionTrack> attentionTrack = modl.getAttentionTrack();
-        highlightButton(attentionTrack, currentpair);
+        highlightButton(attentionTrack, currentpair);//todo: restart/stop this when player guesses
+        // correctly first(currently buggy with countdown and little less buggy without countdown)
         final Runnable r = new Runnable() {
             public void run() {
                 //Only make guess if the card pair was not chosen correctly by the other player
@@ -365,8 +372,40 @@ public class game_activity extends AppCompatActivity implements View.OnClickList
             //}
 
         }
+    }
+    public void Countdown(){
+        DisableButtons();
+        final Handler handler = new Handler();
+        final TextView textView = (TextView) findViewById(R.id.CountdownText);
+        final java.util.concurrent.atomic.AtomicInteger n = new AtomicInteger(3);
+        final Runnable counter = new Runnable() {
+            @Override
+            public void run() {
+                //textView.setVisibility(View.VISIBLE);
+                textView.setText(Integer.toString(n.get()));
+                if(n.getAndDecrement() >= 1 )
+                    handler.postDelayed(this, 1000);
+                else {
+                    textView.setText("");
+                    //EnableButtons();
+                    //Update_Game();
 
-
+                }
+            }
+        };
+        handler.postDelayed(counter, 1000);
+    }
+    private void EnableButtons(){
+        for (int i=0; i<=allButtons.length-1; i++){
+            ImageButton btn = (ImageButton) findViewById(allButtons[0]);
+            btn.setEnabled(true);
+        }
+    }
+    private void DisableButtons(){
+        for (int i=0; i<=allButtons.length-1; i++){
+            ImageButton btn = (ImageButton) findViewById(allButtons[0]);
+            btn.setEnabled(false);
+        }
     }
 
 
