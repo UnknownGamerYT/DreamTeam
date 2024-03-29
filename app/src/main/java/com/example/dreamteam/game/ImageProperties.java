c95b26e3ce0343e8f6418014efc9d237479417d6
PedroLavin <pedrolavin@hotmail.es>
2024-03-29 10:04:28 +0100

    Image Properties Done

---
+++ app/src/main/java/com/example/dreamteam/game/ImageProperties.java
@@ -0,0 +1,25 @@
+package com.example.dreamteam.game;
+
+public class ImageProperties {
+    int image;
+    String[] properties;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String[] getProperties() {
+        return properties;
+    }
+
+    public void setProperties(String[] properties) {
+        this.properties = properties;
+    }
+
+    public ImageProperties() {
+    }
+}
